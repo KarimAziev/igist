b8944d96746c3a77d7a6340b679c69025a6ee840
KarimAziev <karim.aziiev@gmail.com>
2023-04-21 13:33:51 +0300

    Release version 1.1.0
    - Allow sorting gists by modification date.
    - Add variable watcher for `tabulated-list-format` to rerender gists, so `tabulated-list-narrow-current-column` and `tabulated-list-widen-current-column` correctly recalculate indent for files column.
    - Use `igist-message` instead of message, which uses custom variable `igist-message-function`.
    - Update package requirements.

--- igist.el
+++ igist.el
@@ -4,7 +4,7 @@
 
 ;; Author: Karim Aziiev <karim.aziiev@gmail.com>
 ;; URL: https://github.com/KarimAziev/igist
-;; Version: 1.0.0
+;; Version: 1.1.0
 ;; Keywords: tools
 ;; Package-Requires: ((emacs "27.1") (ghub "3.5.6") (transient "0.3.7"))
 
