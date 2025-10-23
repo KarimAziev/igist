0dad57aad72fe3b1084fe4fd19d29275db032fe5
KarimAziev <karim.aziiev@gmail.com>
2025-10-23 11:31:14 +0300

    build: update Emacs version support to 29.1 and above
    Minimal required version of Emacs now is 29.1 due to requirements of ghub.el

--- igist.el
+++ igist.el
@@ -6,7 +6,7 @@
 ;; URL: https://github.com/KarimAziev/igist
 ;; Version: 1.7.0
 ;; Keywords: tools
-;; Package-Requires: ((emacs "27.1") (ghub "4.2.2") (transient "0.8.5"))
+;; Package-Requires: ((emacs "29.1") (ghub "4.2.2") (transient "0.8.5"))
 ;; SPDX-License-Identifier: GPL-3.0-or-later
 
 ;; This file is NOT part of GNU Emacs.
